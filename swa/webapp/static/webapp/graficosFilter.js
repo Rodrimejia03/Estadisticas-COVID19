a39a1b3230cb101faafb8be32746ffd1d8f0ba48
Rodrimejia03 <mm20191@ues.edu.sv>
2023-06-07 21:23:13 -0600

    SideBar modificado, botones reemplazados por <a>

--- swa/webapp/static/webapp/graficosFilter.js
+++ swa/webapp/static/webapp/graficosFilter.js
@@ -2,36 +2,47 @@ const ctx = document.getElementById('myChart');
 const ctx1 = document.getElementById('myChart1');
 const ctx2 = document.getElementById('myChart2');
 
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: labelsU,
-        datasets: [{
-            label: 'Estudiantes inscritos',
-            data: data1,
-            backgroundColor: '#0d9488',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        },  
-    },
-});
 
-new Chart(ctx1, {
-    type: 'pie',
-    data: {
-        labels: labelsU,
-        datasets: [{
-            label: 'Estudiantes Graduados',
-            data: data2,
-            backgroundColor: '#0f766e',
-            hoverOffset: 4
-        }]
-    },
-});
-
+if(true){
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labelsU,
+            datasets: [{
+                label: 'Estudiantes inscritos',
+                data: data1,
+                backgroundColor: '#0d9488',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },  
+        },
+    });
+    
+} else{
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labelsU,
+            datasets: [{
+                label: 'Estudiantes inscritos',
+                data: data1,
+                backgroundColor: '#0d9488',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },  
+        },
+    });
+    
+}
